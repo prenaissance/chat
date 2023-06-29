3c705a9b70f675feb9cf73f99acf3e8e91b5e88f
prenaissance <alexandru.andries@isa.utm.md>
2023-06-29 22:35:21 +0300

    feat: added denormalized 'Conversations' model

--- src/server/api/routers/chat.ts
+++ src/server/api/routers/chat.ts
@@ -18,27 +18,138 @@ export const chatRouter = createTRPCRouter({
 
       const fromId = session.user.id;
 
-      const messageData = await prisma.message.create({
-        data: {
-          content: message,
-          source: MessageSource.User,
-          fromId,
-          targetType,
-          targetId,
-          readBy: {
-            connect: {
-              id: fromId,
+      const messageData = await prisma.$transaction(async (trs) => {
+        const messageData = await trs.message.create({
+          data: {
+            content: message,
+            source: MessageSource.User,
+            fromId,
+            targetType,
+            targetId,
+            readBy: {
+              connect: {
+                id: fromId,
+              },
             },
           },
-        },
-        include: {
-          from: true,
-          targetGroup: true,
-          targetUser: true,
-        },
-      });
+          include: {
+            from: true,
+            targetGroup: true,
+            targetUser: true,
+          },
+        });
 
+        if (messageData.targetUser) {
+          await trs.conversation.upsert({
+            where: {
+              userId_targetType_targetId: {
+                userId: fromId,
+                targetType,
+                targetId,
+              },
+            },
+            create: {
+              userId: fromId,
+              targetType,
+              targetId,
+              lastMessageId: messageData.id,
+              unreadCount: 0,
+            },
+            update: {
+              lastMessageId: messageData.id,
+              unreadCount: 0,
+            },
+          });
+
+          await trs.conversation.upsert({
+            where: {
+              userId_targetType_targetId: {
+                userId: messageData.targetUser.id,
+                targetType,
+                targetId: fromId,
+              },
+            },
+            create: {
+              userId: messageData.targetUser.id,
+              targetType,
+              targetId: fromId,
+              lastMessageId: messageData.id,
+              unreadCount: 1,
+            },
+            update: {
+              lastMessageId: messageData.id,
+              unreadCount: {
+                increment: 1,
+              },
+            },
+          });
+        } else if (messageData.targetGroup) {
+          const group = await trs.group.findUniqueOrThrow({
+            where: {
+              id: messageData.targetGroup.id,
+            },
+            include: {
+              users: true,
+            },
+          });
+
+          const groupUsers = group.users.filter(
+            (user) => user.id !== messageData.fromId
+          );
+
+          await trs.conversation.upsert({
+            where: {
+              userId_targetType_targetId: {
+                userId: fromId,
+                targetType,
+                targetId,
+              },
+            },
+            create: {
+              userId: fromId,
+              targetType,
+              targetId,
+              lastMessageId: messageData.id,
+              unreadCount: 0,
+            },
+            update: {
+              lastMessageId: messageData.id,
+              unreadCount: 0,
+            },
+          });
+
+          await Promise.all(
+            groupUsers.map(async (user) => {
+              await trs.conversation.upsert({
+                where: {
+                  userId_targetType_targetId: {
+                    userId: user.id,
+                    targetType,
+                    targetId: fromId,
+                  },
+                },
+                create: {
+                  userId: user.id,
+                  targetType,
+                  targetId: fromId,
+                  lastMessageId: messageData.id,
+                  unreadCount: 1,
+                },
+                update: {
+                  lastMessageId: messageData.id,
+                  unreadCount: {
+                    increment: 1,
+                  },
+                },
+              });
+            })
+          );
+        }
+
+        return messageData;
+      });
       const messageJSON = JSON.stringify(messageData);
       await redis.publish(RedisChannel.ChatMessages, messageJSON);
+      return messageData;
     }),
 });
