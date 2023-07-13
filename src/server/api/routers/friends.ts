8c58b46d3f9e35a60e039c475f56808dae42e858
prenaissance <alexandru.andries@isa.utm.md>
2023-07-13 23:05:46 +0300

    feat(server > friends): added methods for managing friend requests

--- src/server/api/routers/friends.ts
+++ src/server/api/routers/friends.ts
@@ -75,6 +75,26 @@ export const friendsRouter = createTRPCRouter({
         });
       }
 
+      const receivedFriendRequest = await prisma.friendRequest.findUnique({
+        where: {
+          fromId_toId: {
+            fromId: targetUserId,
+            toId: session.user.id,
+          },
+        },
+      });
+
+      if (receivedFriendRequest) {
+        return await prisma.friendRequest.update({
+          where: {
+            id: receivedFriendRequest.id,
+          },
+          data: {
+            accepted: true,
+          },
+        });
+      }
+
       const friendRequest = await prisma.friendRequest.create({
         data: {
           fromId: session.user.id,
@@ -83,5 +103,97 @@ export const friendsRouter = createTRPCRouter({
       });
 
       return friendRequest;
+    }),
+  cancelFriendRequest: protectedProcedure
+    .input(
+      z.object({
+        targetUserId: z.string().cuid(),
+      })
+    )
+    .mutation(async ({ input, ctx }) => {
+      const { targetUserId } = input;
+      const { session, prisma } = ctx;
+
+      const friendRequest = await prisma.friendRequest.findFirst({
+        where: {
+          fromId: session.user.id,
+          toId: targetUserId,
+          accepted: false,
+        },
+      });
+
+      if (!friendRequest) {
+        throw new TRPCError({
+          message: "You don't have a pending friend request",
+          code: "NOT_FOUND",
+        });
+      }
+    }),
+  rejectFriendRequest: protectedProcedure
+
+    .input(
+      z.object({
+        targetUserId: z.string().cuid(),
+      })
+    )
+    .mutation(async ({ input, ctx }) => {
+      const { targetUserId } = input;
+      const { session, prisma } = ctx;
+
+      const friendRequest = await prisma.friendRequest.findFirst({
+        where: {
+          fromId: targetUserId,
+          toId: session.user.id,
+          accepted: false,
+        },
+      });
+
+      if (!friendRequest) {
+        throw new TRPCError({
+          message: "You don't have a pending friend request",
+          code: "NOT_FOUND",
+        });
+      }
+
+      return await prisma.friendRequest.delete({
+        where: {
+          id: friendRequest.id,
+        },
+      });
+    }),
+
+  acceptFriendRequest: protectedProcedure
+    .input(
+      z.object({
+        targetUserId: z.string().cuid(),
+      })
+    )
+    .mutation(async ({ input, ctx }) => {
+      const { targetUserId } = input;
+      const { session, prisma } = ctx;
+
+      const friendRequest = await prisma.friendRequest.findFirst({
+        where: {
+          fromId: targetUserId,
+          toId: session.user.id,
+          accepted: false,
+        },
+      });
+
+      if (!friendRequest) {
+        throw new TRPCError({
+          message: "You don't have a pending friend request",
+          code: "NOT_FOUND",
+        });
+      }
+
+      return await prisma.friendRequest.update({
+        where: {
+          id: friendRequest.id,
+        },
+        data: {
+          accepted: true,
+        },
+      });
     }),
 });
