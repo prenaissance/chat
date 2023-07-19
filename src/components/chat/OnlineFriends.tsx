e59f63bd154f88acef8b23bd85b418ca77ebfece
prenaissance <alexandru.andries@isa.utm.md>
2023-07-19 23:56:57 +0300

    refactor: separated online friends list to separate component

---
+++ src/components/chat/OnlineFriends.tsx
@@ -0,0 +1,55 @@
+import {
+  type ChakraProps,
+  chakra,
+  useColorModeValue,
+  Text,
+  HStack,
+  IconButton,
+} from "@chakra-ui/react";
+import NextLink from "next/link";
+
+import UserAvatar from "../common/UserAvatar";
+import { api } from "~/utils/api";
+import { AiOutlineSend } from "react-icons/ai";
+
+const OnlineFriends = (props: ChakraProps) => {
+  const friendsQuery = api.friends.getFriends.useQuery();
+  const friends = friendsQuery.data ?? [];
+
+  return (
+    <chakra.aside w={80} {...props}>
+      <Text
+        textAlign="start"
+        fontWeight="bold"
+        letterSpacing={1.2}
+        px={4}
+        py={2}
+      >
+        Friends:
+      </Text>
+      {friends.length ? (
+        friends.map((friend) => (
+          <HStack key={friend.id} w="100%" px={4} py={2} alignItems="center">
+            <UserAvatar user={friend} size="sm" />
+            <Text>{friend.name}</Text>
+            <IconButton
+              ml="auto"
+              as={NextLink}
+              href={`/chat/user/${friend.id}`}
+              aria-label={`Open chat with ${friend.name}`}
+              icon={<AiOutlineSend />}
+              rounded="md"
+              size="sm"
+            />
+          </HStack>
+        ))
+      ) : (
+        <Text px={4} py={2} color={useColorModeValue("gray.700", "gray.300")}>
+          You don't have any friends yet.
+        </Text>
+      )}
+    </chakra.aside>
+  );
+};
+
+export default OnlineFriends;
--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -1,23 +1,9 @@
-import {
-  Flex,
-  HStack,
-  IconButton,
-  Show,
-  Text,
-  chakra,
-  useColorModeValue,
-} from "@chakra-ui/react";
-import { AiOutlineSend } from "react-icons/ai";
-import NextLink from "next/link";
+import { Flex, Show, Text, useColorModeValue } from "@chakra-ui/react";
 
 import ChatLayout from "~/components/chat/ChatLayout";
-import UserAvatar from "~/components/common/UserAvatar";
-import { api } from "~/utils/api";
+import OnlineFriends from "~/components/chat/OnlineFriends";
 
 const Chat = () => {
-  const friendsQuery = api.friends.getFriends.useQuery();
-  const friends = friendsQuery.data ?? [];
-
   return (
     <ChatLayout>
       <Flex h="100%" w="100%">
@@ -31,53 +17,11 @@ const Chat = () => {
           </Text>
         </Flex>
         <Show above="xl">
-          <chakra.aside
-            w={80}
-            h="100%"
-            borderLeft="1px solid"
+          <OnlineFriends
             borderColor={useColorModeValue("gray.400", "gray.600")}
-          >
-            <Text
-              textAlign="start"
-              fontWeight="bold"
-              letterSpacing={1.2}
-              px={4}
-              py={2}
-            >
-              Friends:
-            </Text>
-            {friends.length ? (
-              friends.map((friend) => (
-                <HStack
-                  key={friend.id}
-                  w="100%"
-                  px={4}
-                  py={2}
-                  alignItems="center"
-                >
-                  <UserAvatar user={friend} size="sm" />
-                  <Text>{friend.name}</Text>
-                  <IconButton
-                    ml="auto"
-                    as={NextLink}
-                    href={`/chat/user/${friend.id}`}
-                    aria-label={`Open chat with ${friend.name}`}
-                    icon={<AiOutlineSend />}
-                    rounded="md"
-                    size="sm"
-                  />
-                </HStack>
-              ))
-            ) : (
-              <Text
-                px={4}
-                py={2}
-                color={useColorModeValue("gray.700", "gray.300")}
-              >
-                You don't have any friends yet.
-              </Text>
-            )}
-          </chakra.aside>
+            borderLeft="1px solid"
+            h="100%"
+          />
         </Show>
       </Flex>
     </ChatLayout>
