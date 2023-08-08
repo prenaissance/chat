da936b9a70cee854a222a294bc896442dae0dbae
prenaissance <alexandru.andries@isa.utm.md>
2023-08-08 23:30:06 +0300

    feat(notifications): add preferences store

---
+++ src/stores/notifications.tsx
@@ -0,0 +1,40 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+export type NotificationPreferences = {
+  messages: boolean;
+  friendRequests: boolean;
+};
+
+type NotificationsState = {
+  wasUsedAsked: boolean;
+  enabledPreferences: NotificationPreferences;
+};
+
+type NotificationsActions = {
+  setUserAsked: () => void;
+  setEnabledPreferences: (enabledPreferences: NotificationPreferences) => void;
+};
+
+export type NotificationsStore = NotificationsState & NotificationsActions;
+
+export const useNotificationsStore = create<NotificationsStore>()(
+  persist(
+    (set) => ({
+      wasUsedAsked: false,
+      enabledPreferences: {
+        messages: true,
+        friendRequests: true,
+      },
+      setUserAsked: () => {
+        set({ wasUsedAsked: true });
+      },
+      setEnabledPreferences: (enabledPreferences) => {
+        set({ enabledPreferences });
+      },
+    }),
+    {
+      name: "notifications-store",
+    }
+  )
+);
